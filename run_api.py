dd31964bef0147bd13a1ec94d494814b03dc99ce
yubo2333 <yubo001@e.ntu.edu.sg>
2024-07-13 19:43:18 +0800

    fix bugs for gemini-1.5-pro evaluation

--- run_api.py
+++ run_api.py
@@ -13,6 +13,9 @@ from eval.extract_answer import extract_answer
 from eval.eval_score import eval_score, eval_acc_and_f1, show_results
 
 
+cached_image_list = dict()
+
+
 def encode_image_to_base64(img):
     if img.mode in ('RGBA', 'P'):
         img = img.convert('RGB')
@@ -60,26 +63,37 @@ def process_sample_gpt(sample, args):
     return messages
 
 
-def process_sample_gemini(sample, args):
+def process_sample_gemini(sample, args, mode):
     question = sample["question"]
     doc_name = re.sub("\.pdf$", "", sample["doc_id"]).split("/")[-1]
 
     image_list = list()
     with fitz.open(os.path.join(args.document_path, sample["doc_id"])) as pdf:
-        for index, page in enumerate(pdf[:args.max_pages]):
-            if not os.path.exists(f"./tmp/{doc_name}_{index+1}.png"):
-                im = page.get_pixmap(dpi=args.resolution)
-                im.save(f"./tmp/{doc_name}_{index+1}.png")
-            image_list.append(f"./tmp/{doc_name}_{index+1}.png")
+        if mode=="png":
+            for index, page in enumerate(pdf[:args.max_pages]):
+                if not os.path.exists(f"./tmp/{doc_name}_{index+1}.png"):
+                    im = page.get_pixmap(dpi=args.resolution)
+                    im.save(f"./tmp/{doc_name}_{index+1}.png")
+                image_list.append(Image.open(f"./tmp/{doc_name}_{index+1}.png"))
+        else:
+            if sample["doc_id"] in cached_image_list:
+                image_list = cached_image_list[sample["doc_id"]]
+            else:
+                for index, page in enumerate(pdf[:args.max_pages]):
+                    if not os.path.exists(f"./tmp/{doc_name}_{index+1}.png"):
+                        im = page.get_pixmap(dpi=args.resolution)
+                        im.save(f"./tmp/{doc_name}_{index+1}.png")
+                    image_list.append(genai.upload_file(f"./tmp/{doc_name}_{index+1}.png"))
+                cached_image_list[sample["doc_id"]] = image_list
     
     return [question] + image_list
 
 
-def process_sample(sample, args):
+def process_sample(sample, args, mode="png"):
     if "gpt-4" in args.model_name:
         return process_sample_gpt(sample, args)
     elif "gemini-1.5" in args.model_name:
-        return process_sample_gemini(sample, args)
+        return process_sample_gemini(sample, args, mode)
     else:
         raise AssertionError()
 
@@ -90,7 +104,7 @@ if __name__=="__main__":
     parser.add_argument("--document_path", type=str, default="./data/documents")
     parser.add_argument("--model_name", type=str, default="gpt-4o")
     parser.add_argument("--max_pages", type=int, default=120)
-    parser.add_argument("--resolution", type=int, default=360)
+    parser.add_argument("--resolution", type=int, default=144)
     parser.add_argument("--max_try", type=int, default=10)
     parser.add_argument("--max_tokens", type=int, default=1024)
     parser.add_argument("--temperature", type=float, default=0.0)
@@ -105,6 +119,7 @@ if __name__=="__main__":
     elif "gemini-1.5" in args.model_name:
         import google.generativeai as genai
         client = genai.GenerativeModel(args.model_name)
+        config = genai.types.GenerationConfig(max_output_tokens=args.max_tokens, temperature=args.temperature)
     else:
         raise AssertionError()
 
@@ -136,7 +151,12 @@ if __name__=="__main__":
                         )
                         response = response.choices[0].message.content
                     elif "gemini-1.5" in args.model_name:
-                        response = client.generate_content(messages)
+                        try:
+                            response = client.generate_content(messages, generation_config=config)
+                        except:
+                            print("Payload oversize! Use File API instead.")
+                            messages = process_sample(sample, args, mode="file")
+                            response = client.generate_content(messages, generation_config=config)
                         response.resolve()
                         response = response.text.strip()
                     else:
@@ -151,12 +171,13 @@ if __name__=="__main__":
             sample["response"] = response
             extracted_res = extract_answer(sample["question"], response, prompt)
             sample["extracted_res"] = extracted_res
-            try:
-                pred_ans = extracted_res.split("Answer format:")[0].split("Extracted answer:")[1].strip()
-                score = eval_score(sample["answer"], pred_ans, sample["answer_format"])
-            except:
-                pred_ans = "Failed to extract"
-                score = 0.0
+            # try:
+            print(extracted_res)
+            pred_ans = extracted_res.split("Answer format:")[0].split("Extracted answer:")[1].strip()
+            score = eval_score(sample["answer"], pred_ans, sample["answer_format"])
+            # except:
+            #     pred_ans = "Failed to extract"
+            #     score = 0.0
             sample["pred"] = pred_ans
             sample["score"] = score
 
